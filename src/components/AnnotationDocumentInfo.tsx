4fdd8f1e32a6a7737bc19da60b9a194dc9071933
Alejandro Celaya <alejandro.celaya@hypothes.is>
2025-06-11 10:23:35 +0200

    Add helpers for AnnotationDocumentInfo

--- src/components/AnnotationDocumentInfo.tsx
+++ src/components/AnnotationDocumentInfo.tsx
@@ -1,21 +1,33 @@
 import { Link } from '@hypothesis/frontend-shared';
 
+import type { Annotation } from '../helpers/annotation-metadata';
+import { domainAndTitle } from '../helpers/annotation-metadata';
+
 export type AnnotationDocumentInfoProps = {
-  /** The domain associated with the document */
-  domain?: string;
-  /** A direct link to the document */
-  link?: string;
-  title: string;
+  annotation: Annotation;
 };
+
 /**
  * Render some metadata about an annotation's document and link to it
  * if a link is available.
  */
 export default function AnnotationDocumentInfo({
-  domain,
-  link,
-  title,
+  annotation,
 }: AnnotationDocumentInfoProps) {
+  const { domain, titleText: title, titleLink } = domainAndTitle(annotation);
+  const annotationURL = annotation.links?.html || '';
+  // There are some cases at present in which linking directly to an
+  // annotation's document is not immediately feasible—e.g in an LMS context
+  // where the original document might not be available outside of an
+  // assignment (e.g. Canvas files), and/or wouldn't be able to present
+  // any associated annotations.
+  // For the present, disable links to annotation documents for all third-party
+  // annotations until we have a more nuanced way of making linking determinations.
+  // The absence of a link to a single-annotation view is a signal that this
+  // is a third-party annotation.
+  // Also, of course, verify that there is a URL to the document (titleLink)
+  const link = annotationURL && titleLink ? titleLink : '';
+
   return (
     <div className="flex gap-x-1">
       <div className="text-color-text-light">
