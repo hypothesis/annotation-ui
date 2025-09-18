b30be5643ec51baecaa8f2fbf13210cbeed57fde
Robert Knight <robertknight@gmail.com>
2025-09-18 11:27:32 +0100

    Migrate Tailwind JS config to CSS
    Migrate the Tailwind config used in tests from JS to CSS, as this is the
    preferred way to configure Tailwind v4.

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-import tailwindConfig from '@hypothesis/frontend-shared/lib/tailwind.preset.js';
-
-export default {
-  presets: [tailwindConfig],
-  content: [
-    './src/components/**/*.{js,ts,tsx}',
-    './node_modules/@hypothesis/frontend-shared/lib/**/*.{js,ts,tsx}',
-  ],
-};
